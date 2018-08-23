6475fe96473bb2f47ba80b564169bb294c51a42b
wiljcj <wiljc@gmail.com>
2018-08-23 16:54:04 -0300

    Creating spring boot project with: web, jpa, flyway, h2 and postgresql

---
+++ src/main/java/com/wiliamjcj/wsurvey/WSurveyApiApplication.java
@@ -0,0 +1,12 @@
+package com.wiliamjcj.wsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSurveyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WSurveyApiApplication.class, args);
+	}
+}
