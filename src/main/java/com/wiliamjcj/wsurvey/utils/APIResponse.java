dc068cdc403fcadad876bf92d8a8d1189bea9dcb
wiljcj <wiljc@gmail.com>
2018-08-28 17:41:20 -0300

    Added Swagger for API documentation, added standard response object.

---
+++ src/main/java/com/wiliamjcj/wsurvey/utils/APIResponse.java
@@ -0,0 +1,26 @@
+package com.wiliamjcj.wsurvey.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class APIResponse<T> {
+
+	private T data;
+	private List<String> errors;
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public List<String> getErrors() {
+		if (null == errors)
+			errors = new ArrayList<String>();
+
+		return errors;
+	}
+
+}
